c2d1c4a539517923938e857496c6f6c068245137
TesaTec <144019532+TesaTec@users.noreply.github.com>
2025-10-29 23:51:21 +0100

    Added login screen and basic navigation setup
    The login screen is purely visual right now and does not handle any authentication or similar

---
+++ app/src/main/java/com/example/moneygrab/screens/LoginScreen.kt
@@ -0,0 +1,82 @@
+package com.example.moneygrab.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.moneygrab.ui.theme.MoneyGrabTheme
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier, onLoginClicked: () -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember {mutableStateOf("")}
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Login",
+            modifier = Modifier
+                .padding(bottom = 12.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it},
+            label = { Text("Email")},
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = onLoginClicked,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp)
+        ) {
+            Text("Login")
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    MoneyGrabTheme {
+        LoginScreen(onLoginClicked = {})
+    }
+}
