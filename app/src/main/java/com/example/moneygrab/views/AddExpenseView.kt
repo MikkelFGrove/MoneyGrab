1ce57dddd6bb673eab97a287ad0944006a3716b7
Mikkel <mikkelfgrove@gmail.com>
2025-11-03 21:14:24 +0100

    Added the add expense view type shi

---
+++ app/src/main/java/com/example/moneygrab/views/AddExpenseView.kt
@@ -0,0 +1,73 @@
+package com.example.moneygrab.views
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun PaymentInputView() {
+    var amount by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
+        Button(modifier = Modifier.padding(start= 10.dp, top = 10.dp), onClick = {println("MI BOMBACLAT")}) {
+            Text("Back")
+        }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(64.dp),
+        verticalArrangement = Arrangement.spacedBy(space = 32.dp,
+            alignment = Alignment.CenterVertically),
+
+    ) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            TextField(
+                value = amount,
+                onValueChange = { description = it },
+                modifier = Modifier.weight(1f)
+                    .padding(end = 10.dp),
+                placeholder = { Text("Amount") },
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number), singleLine = true
+            )
+            Text("DKK", style = MaterialTheme.typography.bodyLarge)
+        }
+
+        TextField(value = description, onValueChange = { description = it }, modifier = Modifier.fillMaxWidth(), placeholder = { Text("Add Text") })
+        Button(
+            onClick = {println("MI BOMBACLAT")},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Add Participants")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PaymentInputViewPreview() {
+    MaterialTheme {
+        PaymentInputView()
+    }
+}
