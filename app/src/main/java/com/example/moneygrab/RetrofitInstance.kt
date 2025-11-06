6e68794e921048016afcdbec549d2904cfeeb09e
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-11-06 10:34:16 +0100

    Updated Retrofit

--- app/src/main/java/com/example/moneygrab/RetrofitInstance.kt
+++ app/src/main/java/com/example/moneygrab/RetrofitInstance.kt
@@ -3,7 +3,7 @@ package com.example.moneygrab
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitInstance{
+class RetrofitInstance{
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://example.com/")
